6ac8b0601e163c897c0df3f48db6c4b52f916821
Bogdan Tkachenko <61245273+Korsavabi@users.noreply.github.com>
2020-09-17 16:34:20 +0300

    Delete task-2-exercise.js

--- js/task-2-exercise.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// Задание 2
-// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
-
-const getUsersWithEyeColor = (users, color) => {
-  // твой код
-};
-
-console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
